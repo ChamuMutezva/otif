5c2a89461e9a5bda9c8a87cf7c7ebd75dc043dee
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-08-03 19:41:43 +0200

    back-to-top button - show button when out of 100vh

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const backToTop = document.querySelector(".back-to-top");
 const rootElement = document.documentElement;
 // TODO: hide back-to-top button when scroll is in 100vh//
-/*
-function scrollToTop() {
-    console.log(rootElement.scrollHeight)
-    console.log(rootElement.clientHeight)
-    if (rootElement.clientHeight < 915) {
-        console.log("scroll to top")
-        backToTop.classList.add("hide")
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {  
+    if (
+        document.body.scrollTop > rootElement.clientHeight ||
+        document.documentElement.scrollTop > rootElement.clientHeight
+    ) {
+        backToTop.classList.add("show-btn")         
     } else {
-        backToTop.classList.remove("hide")
+        backToTop.classList.remove("show-btn")            
     }
 }
-document.addEventListener("scroll", scrollToTop)
-*/
